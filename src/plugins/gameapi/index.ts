b101c330bf4abaebe706908b49fbbab03a4cd1a7
ariidesu <shiinazzz.burga@gmail.com>
2025-05-16 01:24:27 +0700

    Move to gameapi

---
+++ src/plugins/gameapi/index.ts
@@ -0,0 +1,16 @@
+import { FastifyPluginAsync } from "fastify";
+import serverRoutes from "./routes/server";
+import rankRoutes from "./routes/rank";
+import unauthenticatedUserRoutes from "./routes/unauthenticatedUser";
+import authenticatedUserRoutes from "./routes/authenticatedUser";
+import shopRoutes from "./routes/shop";
+
+const gameApiApp: FastifyPluginAsync = async (app) => {
+    app.register(unauthenticatedUserRoutes, { prefix: "/user" });
+    app.register(serverRoutes, { prefix: "/server" });
+    app.register(rankRoutes, { prefix: "/server/rank" });
+    app.register(authenticatedUserRoutes, { prefix: "/server/user" });
+    app.register(shopRoutes, { prefix: "/server/shop" });
+};
+
+export default gameApiApp;
