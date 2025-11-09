aef675a2a86012fd1f879d31d1859b9bfea19bb2
ariidesu <shiinazzz.burga@gmail.com>
2025-11-09 10:33:54 +0700

    3.16

--- src/plugins/gameapi/routes/authenticatedUser.ts
+++ src/plugins/gameapi/routes/authenticatedUser.ts
@@ -16,10 +16,10 @@ const authenticatedUserRoutes: FastifyPluginAsync = async (app) => {
             return {
                 status: "OK",
 
-                api_min_ver: 77,
+                api_min_ver: 78,
                 first_login: false,
                 last_device_id: "",
-                latest_ver: 77,
+                latest_ver: 78,
 
                 battle_token: battleToken,
                 web_token: "",
